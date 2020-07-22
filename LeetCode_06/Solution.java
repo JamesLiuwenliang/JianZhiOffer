7d8669399364ab89fba4c9a9766c3d883fa63bf7
JamesLiuwenliang <13114817178@163.com>
2020-07-22 23:45:12 +0900

    Q6

---
+++ LeetCode_06/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public static int[] reversePrint(ListNode head) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        ListNode tmp = head;
+
+        while(tmp != null){
+            linkedList.add(tmp.val);
+            tmp = tmp.next;
+        }
+
+        int total = linkedList.size();
+        int[] res =  new int[total];
+        int index =0;
+        for (int i = total-1 ; i >= 0 ; i--) {
+            res[index] = linkedList.get(i);
+            index++;
+        }
+
+        return res;
+
+    }
+}
