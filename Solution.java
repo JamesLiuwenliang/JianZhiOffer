37041e103a127cf1701751da4b05522c3099b46a
JamesLiuwenliang <13114817178@163.com>
2020-09-14 23:15:38 +0900

    16

---
+++ Solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.math.BigInteger;
+
+// 没有考虑大数的问题
+public class Solution {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(hammingWeight(120));
+    }
+
+    // you need to treat n as an unsigned value
+    public static int hammingWeight(int n) {
+
+        int cnt = 0;
+
+        // Java 提供的方法
+//        cnt = Integer.bitCount(n);
+
+        while(n != 0){
+            if((n&1) == 1){
+                cnt ++;
+            }
+            
+            // Java右移补0的符号是 >>>
+            // 如果用>> 面对负数的时候会重复补1
+            n = n >>> 1;
+        }
+
+
+        return cnt ;
+    }
+
+}
