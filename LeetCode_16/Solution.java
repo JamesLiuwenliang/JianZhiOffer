390934a11336c7f27bb1bb92dbdac13d2f5ce659
JamesLiuwenliang <13114817178@163.com>
2020-09-15 22:26:29 +0900

    16

--- LeetCode_16/Solution.java
+++ LeetCode_16/Solution.java
@@ -3,26 +3,52 @@ import java.util.*;
 public class Solution {
     
     public static void main(String[] args) {
+        /**
+         * int 的范围是 -2147483648 ~ 2147483647,所以-2147483648的abs结果还是-2147483648。最好用long类型
+         */
         System.out.println(myPow(1.0, -2147483648));
     }
 
-    public static double myPow(double x, int n) {
+    public static double myPow(double x, long n) {
 
-        if(n==0) {
+        double result = 0.0;
+        if(Double.compare(x,0.0) == 0){
+            return 0.0;
+        }
+
+        long abs_n = Math.abs(n) ;
+
+        result = myPowEngine(x,abs_n);
+
+        if(n <0 ){
+            result = 1.0/result;
+        }
+
+        return result;
+
+
+
+    }
+
+    // 在这个函数上加速
+    private static double myPowEngine(double x ,long n){
+
+        if(n == 0){
             return 1;
         }
-        if(n==-1){
-            return 1.0/x;
+        if(n== 1){
+            return x;
         }
-        
-        if((n&0x1) == 1) {
-            return myPow(x*x, n>>1)*x;
-        }
-        else {
-            return myPow(x*x, n>>1);
+
+        double result = myPowEngine(x,n/2);
+        result *= result;
+
+        if((n & 0x1) == 1){
+            result *= x;
         }
 
 
+        return result;
     }
 
     
