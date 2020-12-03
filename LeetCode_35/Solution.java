a30668997102ea7a1f1d78fc594825e7ce098535
JamesLiuwenliang <13114817178@163.com>
2020-12-03 21:33:57 +0900

    35

---
+++ LeetCode_35/Solution.java
@@ -0,0 +1,116 @@
+import java.util.HashMap;
+
+public class Solution {
+
+    static class Node35 {
+        int val;
+        Node35 next;
+        Node35 random;
+
+        public Node35(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Node35 node01 = new Node35(7);
+        Node35 node02 = new Node35(13);
+        Node35 node03 = new Node35(11);
+        Node35 node04 = new Node35(10);
+        Node35 node05 = new Node35(1);
+
+        node01.next = node02;  node02.next = node03;  node03.next = node04;  node04.next = node05;
+        node02.random = node01;node03.random = node05;node04.random = node03;node05.random = node01;
+
+        copyRandomList(node01);
+
+
+
+    }
+
+    public static Node35 copyRandomList(Node35 head) {
+
+        if(head == null){
+            return null;
+        }
+
+        Node35 resultHeadNode = new Node35(head.val);
+
+        if(head.next == null){
+            int index = indexFind(head,head.random);
+            if(index==-1){
+                return resultHeadNode;
+            }else if(index == 0) {
+                resultHeadNode.random = resultHeadNode;
+            }
+
+            return resultHeadNode;
+        }
+
+        Node35 nodeOriginP = head;
+        Node35 resultNodeP = resultHeadNode;
+        HashMap<Node35,Integer> hashMap = new HashMap<>();
+        hashMap.put(resultHeadNode,indexFind(head,nodeOriginP.random));
+
+        while(nodeOriginP.next != null){
+
+            resultNodeP.next = new Node35(nodeOriginP.next.val);
+            resultNodeP = resultNodeP.next;
+            nodeOriginP = nodeOriginP.next;
+
+            hashMap.put(resultNodeP,indexFind(head,nodeOriginP.random));
+        }
+
+        resultNodeP = resultHeadNode;
+        nodeOriginP = head;
+
+        Node35 nodeOuterPointer = resultNodeP;
+        while(nodeOuterPointer != null){
+
+            int index = hashMap.get(nodeOuterPointer);
+            if(index==-1){
+                nodeOuterPointer = nodeOuterPointer.next;
+                continue;
+            }
+
+            Node35 nodeTmpPointer = resultHeadNode;
+
+            for (int i = 0; i < index; i++) {
+                nodeTmpPointer = nodeTmpPointer.next;
+            }
+
+            nodeOuterPointer.random = nodeTmpPointer;
+            nodeOuterPointer = nodeOuterPointer.next;
+
+        }
+
+
+
+        return resultHeadNode;
+
+
+    }
+
+    private static int indexFind(Node35 head,Node35 nodeFind){
+
+        if(nodeFind == null){
+            return -1;
+        }
+
+        int index = 0;
+        Node35 tmp = head;
+        while(tmp != null){
+            if(tmp == nodeFind){
+                break;
+            }
+            tmp = tmp.next;
+            ++index;
+        }
+        return index;
+
+    }
+
+}
