39b12a160d8444e47cc8d9954475cb95713aaa5d
JamesLiuwenliang <13114817178@163.com>
2020-12-14 00:28:23 +0900

    53

--- LeetCode_53/Solution.java
+++ LeetCode_53/Solution.java
@@ -1,17 +1,17 @@
-
-
-
 public class Solution {
 
     public static void main(String[] args) {
 
         int[] nums = {2,2};
+        System.out.println(search_Q1(nums,2));
 
-        System.out.println(search(nums,2));
+        int[] nums = {0,1,2,3,4,5,6,7,9};
+//        int[] nums = {1,2};
+        System.out.println(missingNumber_Q2(nums));
 
     }
 
-    public static int search(int[] nums, int target) {
+    public static int search_Q1(int[] nums, int target) {
 
         if(nums.length == 0|| nums == null){
             return 0;
@@ -53,7 +53,7 @@ public class Solution {
     }
 
     // 这个二分写的比我的强
-    public static int search_LeetCode(int[] nums, int target) {
+    public static int search_LeetCode_Q1(int[] nums, int target) {
         int left =0,right = nums.length-1;
         int count = 0;
         while(left<right){
@@ -67,6 +67,35 @@ public class Solution {
             count++;
         return count;
     }
+
+    public static int missingNumber_Q2(int[] nums) {
+
+        if(nums.length == 0 || nums == null){
+            return -1;
+        }
+
+        if(nums.length-1 == nums[nums.length-1]){
+            return nums.length;
+        }
+
+
+        int left = 0,right = nums.length-1;
+
+        while(left < right){
+
+            int mid = (left+right)/2;
+
+            if(nums[mid] == mid){
+                left = mid+1;
+            }else{
+                right = mid;
+            }
+        }
+
+        return nums.length == nums[left]  ?   left  : right ;
+
+    }
+
 
 }
 
