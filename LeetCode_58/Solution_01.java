b092688c7f728d12ba119763a6732ecd4ae55cf4
JamesLiuwenliang <13114817178@163.com>
2021-01-02 17:25:22 +0900

    5801

---
+++ LeetCode_58/Solution_01.java
@@ -0,0 +1,44 @@
+public class Solution_01 {
+
+
+    public static void main(String[] args) {
+
+
+
+
+        System.out.println(reverseWords("  hello world!  "));
+
+
+    }
+
+    public static  String reverseWords(String s) {
+
+
+        if(s == null){
+            return null;
+        }
+
+        s = s.trim();
+        if(s.length() == 0){
+            return s;
+        }
+
+        String[] strings = s.split(" ");
+
+        StringBuffer stringBuffer = new StringBuffer();
+
+        for (int i = strings.length-1; i >= 0 ; i--) {
+            if (strings[i].length() == 0) {
+                continue;
+            }
+
+            stringBuffer.append(strings[i]+" ");
+        }
+
+        if(stringBuffer.charAt(stringBuffer.length()-1) == ' '){
+            stringBuffer.deleteCharAt(stringBuffer.length()-1);
+        }
+
+        return stringBuffer.toString();
+    }
+}
