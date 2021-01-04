24428279d72df5cbefddecfffe5b55d8f0eb6f6a
JamesLiuwenliang <13114817178@163.com>
2021-01-04 19:56:41 +0900

    re64

--- LeetCode_64/Solution.java
+++ LeetCode_64/Solution.java
@@ -1,9 +1,19 @@
 
-
+// 这题其实挺难的，可以用做复习Cpp的特性的题
 public class Solution {
+    
+    public static int res = 0;
 
     public static void main(String[] args) {
         System.out.println(sumNums(9));
+
+    }
+
+    public static int sumNums_LeetCode(int n){
+        boolean x = n > 1 && sumNums_LeetCode(n - 1) > 0;
+        res += n;
+
+        return res;
 
     }
 
