71092f8bbb34ae6d81a48fc58dffc97e01b2a610
JamesLiuwenliang <13114817178@163.com>
2021-01-05 22:33:26 +0900

    re67

--- LeetCode_67/Solution.java
+++ LeetCode_67/Solution.java
@@ -1,14 +1,20 @@
-
 import java.util.LinkedList;
 
 public class Solution {
 
+    // 全局变量判断是真的0还是是违法输入
+    private static boolean ErrorisFalse_0isTrue = true;
 
     public static void main(String[] args) {
 
+        String str = "-91283472332";
+        String str02 = "";
+        String str03 = " ";
 
-        System.out.println(strToInt("-2147483648"));
-//        "-2147483647"
+        System.out.println(strToInt(str03));
+
+        System.out.println(strToInt_Book(str03));
+
 
     }
     public static  int strToInt(String str){
@@ -16,10 +22,13 @@ public class Solution {
             return 0;
         }
 
+
+
         str = str.trim();
         if(str.length() == 0){
             return 0;
         }
+
         int result = 0;
         int carry = 0;
         LinkedList<Character> linkedList = new LinkedList<>();
@@ -77,6 +86,64 @@ public class Solution {
         return result;
 
 
+
+
+
+    }
+
+    public static  int strToInt_Book(String str) {
+        if(str == null ){
+            ErrorisFalse_0isTrue = false;
+            return 0;
+        }
+
+        if(str.length() == 0){
+            return 0;
+        }
+
+        char[] chars = str.toCharArray();
+        int indexChars = 0;
+
+
+        while(indexChars < str.length() && chars[indexChars] == ' '){
+            indexChars++;
+        }
+
+        int sign = 1;
+
+        if(indexChars < str.length() && chars[indexChars] == '+'){
+            indexChars++;
+        }else if(indexChars < str.length() && chars[indexChars] == '-'){
+            indexChars++;
+            sign = -1;
+        }
+
+        long result = 0;
+
+        while(indexChars != str.length()){
+
+            if(chars[indexChars] >= '0' && chars[indexChars]<= '9'){
+
+                result = result*10 + sign * (chars[indexChars] - '0');
+
+                // 边界条件的判断
+                if( (sign == -1 && result < Integer.MIN_VALUE) || (sign == 1 && result > Integer.MAX_VALUE)){
+
+                    return sign == -1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+                }
+
+                indexChars++;
+
+
+            }else{
+
+                break;
+            }
+
+
+        }
+
+        return (int)result;
 
 
 
